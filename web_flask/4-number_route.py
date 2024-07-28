95526ec7e642e162113c767a89e43edccb4bc64a
= <=>
2024-07-28 07:55:48 -0700

    Number Display

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Script that start we application and display "Hello HBNB"
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def home():
+    """Display Hello HBNB"""
+
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Display HBNB"""
+
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """
+    Displays C followed by the value of the text variable
+    Replaces any underscores in <text> with a space
+    """
+    text_display = text.replace("_", " ")
+    return "C {}".format(text_display)
+
+
+@app.route("/python", defaults={'text': 'is_cool'})
+@app.route("/python/<text>")
+def python(text):
+    """
+    Displays Python followed by the value of text variable
+    Replaces any underscores in <text> with a space
+    """
+
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>")
+def number(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
